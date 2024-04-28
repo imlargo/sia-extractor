2236f8f583de47e3c31fffbafac16e687a6729e6
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-04-28 16:32:10 +0000

    _

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const { getCarreras } = require("./src/util.js");
-const { getListMaterias, getMateria, getAllMaterias } = require("./src/main.js");
+const CODIGOS = require("./src/codigos.json");
+const child = require('child_process');
 
-// import CODIGOS from "./src/codigos.json";
+const CONFIG = {
+	cantidad: 5,
+
+};
+
+function agrupar() {
+	// Unir todas las carreras en un solo objeto
+	const carreras = Object.values(CODIGOS).reduce((acc, curr) => ({ ...acc, ...curr }) );
+	const entries = Object.entries(carreras)
+
+	// Dividir las carreras en arrays de 5 carreras
+	const grupos = [];
+	let grupo = [];
+	for (let i = 0; i < entries.length; i++) {
+		const [nombre, codigo] = entries[i];
+		grupo.push({nombre, codigo});
+		if (grupo.length === CONFIG.cantidad) {
+			grupos.push(grupo);
+			grupo = [];
+		}
+	}
+
+	return grupos
+}
+
 
 async function main() {
-	const data = await getAllMaterias("0-6-5-13");
-	console.log(data);
+	const grupos = agrupar();
+
+	for (const grupo of grupos) {
+		// console.log(grupo);
+		const proceso = child.spawn(
+			'node', ['./src/worker-grupo.js', JSON.stringify(grupo)
+		],
+			{ detached: true }
+		);
+
+		proceso.stdout.on('data', (data) => {
+			console.log(`stdout: ${data}`);
+		});
+	}
+	
 }
 
 main();
