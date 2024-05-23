c78b23667bd6b28d6a7e9e73532826fdb7894c2c
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-05-23 13:09:29 -0500

    scrapp pre requisitos

--- src/util.js
+++ src/util.js
@@ -226,6 +226,20 @@ function procesarMateria() {
 		grupos.push(grupo);
 	}
 
+
+	const prerrequisitos = elementosGrupo[elementosGrupo.length-1].nextElementSibling;
+	const hasRequisitos = prerrequisitos.textContent.includes("Prerrequisitos");
+
+	const requisitos = hasRequisitos ? prerrequisitos.querySelectorAll(".borde.salto.af_panelGroupLayout").map(container => {
+		const listado = Array.from(container.firstChild.childNodes);
+		const datos = listado.shift();
+		return listado.map(req => Array.from(req.firstChild.childNodes).map(node => node.textContent))
+	}) : 0;
+
+	if (requisitos) {
+		console.log(requisitos);
+	}
+	
 	// Crear objeto para la materia con los datos procesados
 	const materia = {
 		nombre: nombreMateria,
