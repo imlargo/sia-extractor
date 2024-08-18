5791d08e3eb7593f3a52d4584ddbf8cb2f60a4f3
Juan Carlos Largo <jlargob@unal.edu.co>
2024-08-18 13:42:52 -0500

    move mongodb uri to env

--- old/src/deploy.js
+++ old/src/deploy.js
@@ -1,6 +1,7 @@
 const DATA = require("./data.json");
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://imlargo:VFxwJVY1Ykued4cg@asignaturas.vs2qizj.mongodb.net/?retryWrites=true&w=majority&appName=asignaturas";
+
+const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri, {
 	serverApi: {
