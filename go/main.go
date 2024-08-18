957bd5857ea9eebf92f061e04c11eee4c7026f1a
Juan Carlos Largo <jlargob@unal.edu.co>
2024-08-18 09:15:44 -0500

    _

--- go/main.go
+++ go/main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math"
 	"os"
 	"sia-extractor/core"
 	"sync"
 	"time"
 )
 
+const (
+	cantidad_por_grupo int = 4
+)
+
 func main() {
 
 	// core.CreatePathsCarreras()
+
+	fmt.Println(len(os.Args), os.Args)
 
 	initTime := time.Now()
 
@@ -21,16 +28,46 @@ func main() {
 	println("......................................................")
 	fmt.Printf("Tiempo de ejecuciónnnnnnnnnnn final: %v\n", time.Since(initTime))
 
+	// group()
+
+}
+
+func group() {
+	var listadoCarreras []map[string]string
+
+	contentCarreras, _ := os.ReadFile(core.Path_Carreras)
+	json.Unmarshal(contentCarreras, &listadoCarreras)
+
+	stacks := int(math.Ceil(float64(len(listadoCarreras)) / float64(cantidad_por_grupo)))
+
+	println("Cantidad de stacks: ", (stacks))
+
+	var grupos [][]map[string]string
+	for i := 0; i < stacks; i++ {
+		var grupo []map[string]string
+
+		for j := 0; j < cantidad_por_grupo; j++ {
+			if (i*cantidad_por_grupo)+j < len(listadoCarreras) {
+				grupo = append(grupo, listadoCarreras[(i*cantidad_por_grupo)+j])
+			}
+		}
+
+		grupos = append(grupos, grupo)
+	}
+
+	dataGruposJSON, _ := json.Marshal(grupos)
+	os.WriteFile("grupos.json", dataGruposJSON, 0644)
+
 }
 
 func extraerTodo() {
 	var listadoCarreras []map[string]string
 
-	contentCarreras, _ := os.ReadFile("carreras.json")
+	contentCarreras, _ := os.ReadFile(core.Path_Carreras)
 	json.Unmarshal(contentCarreras, &listadoCarreras)
 
 	var wg sync.WaitGroup
-	for _, carrera := range listadoCarreras[0:1] {
+	for _, carrera := range listadoCarreras[0:4] {
 
 		wg.Add(1)
 
